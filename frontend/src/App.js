fd1fdd4f30dee38ba91b646e7e5852fdc9e3fbbe
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2020-05-22 17:48:31 +0100

    Added lazy loading to the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
-import React, { useContext } from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Components
 import Header from "./components/Header/index";
 // import Footer from './components/Footer/index';
 
-// Pages
-import Home from "./pages/Home/index";
-import Login from "./pages/Login/index";
-import Signin from "./pages/Signin/index";
-import User from "./pages/User/index";
 
 // Context
 import GifsProvider from "./store/GifsProvider";
@@ -18,11 +13,18 @@ import AuthProvider from "./store/AuthProvider";
 // Routes
 import PrivateRoute from './store/routes'
 
+// Pages
+const Home = lazy(() => import ('./pages/Home/index'))
+const Login = lazy(() => import ('./pages/Login/index'))
+const Signin = lazy(() => import ('./pages/Signin/index'))
+const User = lazy(() => import ('./pages/User/index'))
+
 function App() {
   return (
     <GifsProvider>
       <AuthProvider>
         <Router>
+          <Suspense fallback={<div>Loading...</div>}>
           <div className="App">
             <Header />
             <Route path="/" exact component={Home} />
@@ -31,6 +33,7 @@ function App() {
             <PrivateRoute path="/user" component={User} />
             {/* <Footer /> */}
           </div>
+          </Suspense>
         </Router>
       </AuthProvider>
     </GifsProvider>
