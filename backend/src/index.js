d23aa418a0cc05ab6703d1fc662f5a6a173af165
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2020-06-19 01:29:36 +0100

    changed some gitignore info in order to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const path = require('path');
+const path = require('path'); 
 const mongoose = require('./database');
 const cors = require('cors');
 
---
+++ frontend/build/precache-manifest.4ef550767f48336b130c99383effbe69.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e71219c5aabe6076e43e80345761bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "701509012a234d6ce4b8",
+    "url": "/static/js/0.2577b2d9.chunk.js"
+  },
+  {
+    "revision": "ea9c70c069681419fb31",
+    "url": "/static/js/3.72234f53.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/3.72234f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f23da4c967ec08ceade",
+    "url": "/static/js/4.3e184095.chunk.js"
+  },
+  {
+    "revision": "d5219574840b25208f72",
+    "url": "/static/js/5.9544df1c.chunk.js"
+  },
+  {
+    "revision": "3bdaf6dac07aefaed569",
+    "url": "/static/js/6.8d0bf052.chunk.js"
+  },
+  {
+    "revision": "f7068f22b0d58d9207a0",
+    "url": "/static/js/7.23118691.chunk.js"
+  },
+  {
+    "revision": "f2ef8ca92a2d4f044ac7",
+    "url": "/static/js/main.a87eaaad.chunk.js"
+  },
+  {
+    "revision": "395478e600eeb394e85d",
+    "url": "/static/js/runtime-main.4f6ab4ba.js"
+  },
+  {
+    "revision": "fbd34f0569f515753de5e79f02fa5217",
+    "url": "/static/media/arrow.fbd34f05.svg"
+  },
+  {
+    "revision": "b5bbf8ab06d75c6e9e9af177c010b6d6",
+    "url": "/static/media/ghost-logo.b5bbf8ab.svg"
+  }
+]);
